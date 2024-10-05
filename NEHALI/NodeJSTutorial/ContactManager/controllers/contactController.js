a74be8b0f7b380965de04a4834f560608b424d95
Nehali <bhaleraonehali7@gmail.com>
2024-10-05 19:34:54 +0530

    middleware setup done

--- NEHALI/NodeJSTutorial/ContactManager/controllers/contactController.js
+++ NEHALI/NodeJSTutorial/ContactManager/controllers/contactController.js
@@ -10,6 +10,12 @@ const getContacts=(req,res)=>{
 //@route POST /api/contacts
 //@access public
 const createContact=(req,res)=>{
+    console.log("req",req.body)
+    const{name,email,phone}=req.body;
+    if(!name || !email || !phone){
+        res.status(400);
+        throw new Error("All fields are mandatory")
+    }
     res.status(201).json({message:"Create Contact"})
 }
 
--- NEHALI/NodeJSTutorial/ContactManager/server.js
+++ NEHALI/NodeJSTutorial/ContactManager/server.js
@@ -1,15 +1,19 @@
 const express=require("express");
+const errorHandler = require("./middlerware/errorHandler");
 const dotenv=require("dotenv").config();
 
 
 const app=express();
 
 const port=process.env.PORT || 5000;
-console.log(port)
+
+
+app.use(express.json());
 
 
 app.use("/api/contacts",require('./routes/contactRoutes'));
 
+app.use(errorHandler)
 app.listen(port,()=>{
     console.log('Server running on port',port);
 })
