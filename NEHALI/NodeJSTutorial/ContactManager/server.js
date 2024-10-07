6a4e4f6b714462d45321078a0dfaca613239456e
Nehali <bhaleraonehali7@gmail.com>
2024-10-07 13:11:56 +0530

    authentication route added and jwt validation added and get current user converted to private

--- NEHALI/NodeJSTutorial/ContactManager/server.js
+++ NEHALI/NodeJSTutorial/ContactManager/server.js
@@ -13,6 +13,7 @@ app.use(express.json());
 
 
 app.use("/api/contacts",require('./routes/contactRoutes'));
+app.use("/api/users",require('./routes/userRoutes'));
 
 app.use(errorHandler)
 app.listen(port,()=>{
